466d75696625157a1d88613ac3ff57dab4cc4f97
aultimus <matthew.ault@cocoon.life>
2017-01-12 21:43:56 +0000

    s1c5: add solution

--- util_test.go
+++ util_test.go
@@ -34,3 +34,17 @@ func TestC2(t *testing.T) {
 	actual := hex.EncodeToString(bOut)
 	a.Equal(expected, actual)
 }
+
+func TestC5(t *testing.T) {
+	a := assert.New(t)
+
+	in1 := `Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal`
+	in2 := "ICE"
+
+	expected := `0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f`
+
+	actualBytes := Xor([]byte(in1), []byte(in2))
+	actualStr := hex.EncodeToString(actualBytes)
+	a.Equal(expected, actualStr)
+}
