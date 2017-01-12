780558a4dc8f3235b109e64c8426ed41c3982b9f
aultimus <matthew.ault@cocoon.life>
2017-01-12 20:22:53 +0000

    Move bruteforce xor single char decoder from s1c3 to util

--- s1c3/main.go
+++ s1c3/main.go
@@ -2,56 +2,16 @@ package main
 
 import (
 	"encoding/hex"
-	"strings"
 
 	"github.com/aultimus/cryptopals"
 	"github.com/davecgh/go-spew/spew"
 )
 
-type result struct {
-	plaintext string
-	cypher    int
-	score     int
-}
-
 func main() {
-
 	in := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 	bIn1, err := hex.DecodeString(in)
 	if err != nil {
 		panic(err)
 	}
-	topResult := &result{}
-	n := 128
-	// cycle through possible cyphers
-	for i := 0; i < n; i++ {
-		// construct equal sized cypher array for passing to Xor
-		bIn2 := make([]byte, len(bIn1))
-		for j := 0; j < len(bIn1); j++ {
-			bIn2[j] = byte(i)
-		}
-		s := string(cryptopals.Xor(bIn1, bIn2))
-		r := &result{
-			plaintext: s,
-			cypher:    i,
-			score:     score(s),
-		}
-		if r.score > topResult.score {
-			topResult = r
-		}
-	}
-	spew.Dump(topResult)
-}
-
-func score(s string) int {
-	popular := "uldrhsnioate" // "etaoinshrdlu" // reversed
-
-	var total int
-	for i := 0; i < len(s); i++ {
-		subscore := strings.Index(popular, string(s[i]))
-		if subscore != -1 {
-			total += subscore
-		}
-	}
-	return total
+	spew.Dump(cryptopals.BruteforceXOR(bIn1))
 }
--- util.go
+++ util.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"strings"
 )
 
 // HexToBase64 takes a hex encoded byte array and returns a base 64 encoded
@@ -40,4 +41,50 @@ func Xor(b1, b2 []byte) []byte {
 		out[i] = b1[i] ^ b2[i]
 	}
 	return out
+}
+
+type Result struct {
+	Plaintext string
+	Cypher    int
+	Score     int
+}
+
+// BruteforceXOR searches for a single character XOR cypher that yields the
+// most likely plaintext
+func BruteforceXOR(bIn1 []byte) *Result {
+	topResult := &Result{}
+	// cycle through possible cyphers
+	for i := 0; i < 128; i++ {
+		// construct equal sized cypher array for passing to Xor
+		bIn2 := make([]byte, len(bIn1))
+		for j := 0; j < len(bIn1); j++ {
+			bIn2[j] = byte(i)
+		}
+		s := string(Xor(bIn1, bIn2))
+		r := &Result{
+			Plaintext: s,
+			Cypher:    i,
+			Score:     ScorePlaintext(s),
+		}
+		if r.Score > topResult.Score {
+			topResult = r
+		}
+	}
+	return topResult
+
+}
+
+// ScorePlaintext scores a string for confidence that is plaintext, the higher the score,
+// the higher the confidence
+func ScorePlaintext(s string) int {
+	popular := "uldrhsnioate" // "etaoinshrdlu" // reversed
+
+	var total int
+	for i := 0; i < len(s); i++ {
+		subscore := strings.Index(popular, string(s[i]))
+		if subscore != -1 {
+			total += subscore
+		}
+	}
+	return total
 }
