65387cf2de73541134e6a8ae5b06c621515dc21c
aultimus <matthew.ault@cocoon.life>
2017-01-13 17:13:58 +0000

    s1c6: analyse more data when determining keysize, increase confidence

--- s1c6/main.go
+++ s1c6/main.go
@@ -8,26 +8,32 @@ import (
 )
 
 func main() {
+	// Read Data
 	b64, err := ioutil.ReadFile("6.txt")
 	if err != nil {
 		panic(err.Error())
 	}
 
+	// Decode from base64
 	b, err := cryptopals.Base64Decode(b64)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	shortestDistance := 1000 // suitably large
+	// Determine keysize
+	shortestDistance := 1000.0 // suitably large
 	likelyKeysize := -1
 	for keysize := 2; keysize < 41; keysize++ {
 		b1 := b[:keysize]
 		b2 := b[keysize : keysize*2]
-		normDistance := cryptopals.HammingDistance(b1, b2) / keysize
+		b3 := b[keysize*2 : keysize*3]
+		b4 := b[keysize*3 : keysize*4]
+		normDistance := cryptopals.HammingDistance(b1, b2, b3, b4) / float64(keysize)
+		//fmt.Printf("keysize %d, distance %f\n", keysize, normDistance)
 		if normDistance < shortestDistance {
 			shortestDistance = normDistance
 			likelyKeysize = keysize
 		}
 	}
-	fmt.Printf("Likely keysize %d has normalised hamming distance of %d\n", likelyKeysize, shortestDistance)
+	fmt.Printf("Likely keysize %d has normalised hamming distance of %f\n", likelyKeysize, shortestDistance)
 }
