42cf54bf9efe6b5350f3740ca0fb800d3acfcda9
aultimus <matthew.ault@cocoon.life>
2017-01-15 20:21:48 +0000

    util: export BreakRepeatingKeyXOR function

--- s1c6/main.go
+++ s1c6/main.go
@@ -20,50 +20,6 @@ func main() {
 		panic(err.Error())
 	}
 
-	keysize, distance := cryptopals.DetermineKeysize(b)
-	fmt.Printf("Likely keysize %d has normalised hamming distance of %f\n",
-		keysize, distance)
-
-	blocks := makeBlocks(b, keysize, keysize)
-	transposed := transposeBlocks(blocks)
-
-	cypher := make([]byte, keysize)
-	for i, block := range transposed {
-		result := cryptopals.BruteforceXOR(block)
-		cypher[i] = byte(result.Cypher)
-	}
-
-	plaintext := string(cryptopals.Xor(b, cypher))
+	plaintext := string(cryptopals.BreakRepeatingKeyXOR(b))
 	fmt.Println(plaintext)
-}
-
-// TODO: test
-func transposeBlocks(blocks [][]byte) [][]byte {
-	l := len(blocks)
-	if l != len(blocks[0]) {
-		panic(fmt.Sprintf("unable to transpose non square matrix (%d by %d)",
-			l, len(blocks[0])))
-	}
-	transposed := make([][]byte, l)
-	for i := 0; i < l; i++ {
-		transposed[i] = make([]byte, l)
-	}
-
-	for i := 0; i < l; i++ {
-		for j := 0; j < l; j++ {
-			transposed[i][j] = blocks[j][i]
-		}
-	}
-	return transposed
-}
-
-// TODO: test
-func makeBlocks(b []byte, blocksize, numBlocks int) [][]byte {
-	// make blocksize blocks of blocksize length
-	blocks := make([][]byte, numBlocks)
-	for i := 0; i < numBlocks; i++ {
-		blocks[i] = make([]byte, blocksize)
-		blocks[i] = b[blocksize*i : (blocksize*i)+blocksize]
-	}
-	return blocks
 }
--- util.go
+++ util.go
@@ -188,3 +188,52 @@ func DetermineKeysize(b []byte) (int, float64) {
 	}
 	return likelyKeysize, shortestDistance
 }
+
+//BreakRepeatingKeyXOR ...
+func BreakRepeatingKeyXOR(b []byte) []byte {
+	keysize, distance := DetermineKeysize(b)
+	fmt.Printf("Likely keysize %d has normalised hamming distance of %f\n",
+		keysize, distance)
+
+	blocks := makeBlocks(b, keysize, keysize)
+	transposed := transposeBlocks(blocks)
+
+	cypher := make([]byte, keysize)
+	for i, block := range transposed {
+		result := BruteforceXOR(block)
+		cypher[i] = byte(result.Cypher)
+	}
+
+	return Xor(b, cypher)
+}
+
+// TODO: test
+func transposeBlocks(blocks [][]byte) [][]byte {
+	l := len(blocks)
+	if l != len(blocks[0]) {
+		panic(fmt.Sprintf("unable to transpose non square matrix (%d by %d)",
+			l, len(blocks[0])))
+	}
+	transposed := make([][]byte, l)
+	for i := 0; i < l; i++ {
+		transposed[i] = make([]byte, l)
+	}
+
+	for i := 0; i < l; i++ {
+		for j := 0; j < l; j++ {
+			transposed[i][j] = blocks[j][i]
+		}
+	}
+	return transposed
+}
+
+// TODO: test
+func makeBlocks(b []byte, blocksize, numBlocks int) [][]byte {
+	// make blocksize blocks of blocksize length
+	blocks := make([][]byte, numBlocks)
+	for i := 0; i < numBlocks; i++ {
+		blocks[i] = make([]byte, blocksize)
+		blocks[i] = b[blocksize*i : (blocksize*i)+blocksize]
+	}
+	return blocks
+}
