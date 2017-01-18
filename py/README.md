911eaceaf67eb6bbb94ef02565cac388902f5882
aultimus <matthew.ault@cocoon.life>
2017-01-18 19:40:14 +0000

    python: add first two challenges implemented in python

---
+++ py/README.md
@@ -0,0 +1,4 @@
+cryptopals
+==========
+
+This directory contains my python solutions to the cryptopals challenges. I was motivated to switch from golang to python in order to trade machine efficiency for speed of implementation once it became apparent that the efficiency constraints for these challenges were not heavy.
---
+++ py/crypto.py
@@ -0,0 +1,13 @@
+
+def xor(b1, b2):
+	l = len(b1)
+	if len(b2) > l:
+		raise ValueError("Xor does not accept b2 longer than b1, args len(b1)=%d, len(b2)=%d" % (len(b1), len(b2)))
+	out = bytearray(l)
+	b2Index = 0
+	for i in range(0, l):
+		out[i] = b1[i] ^ b2[b2Index]
+		b2Index+=1
+		if b2Index == len(b2):
+			b2Index = 0
+	return out
